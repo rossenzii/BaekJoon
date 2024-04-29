43f5b0cb1f03ba0ccf271b70a38aabc15edef190
minjiggy <112603809+MaRoili@users.noreply.github.com>
2024-04-29 11:23:32 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main(void){
+    fastio;
+    int N; cin>>N;
+    while(N--){
+        string a,b;
+        cin>>a>>b;
+        vector<int> cnt(26);
+        for(int i=0; i<a.size(); i++) cnt[a[i]-'a']++;
+        for(int i=0; i<b.size(); i++) cnt[b[i]-'a']--;
+        bool flag =1;
+        for(int i=0; i<26; i++) if(cnt[i]) flag=0;
+        cout<<(flag? "Possible":"Impossible")<<'\n';
+    }
+    
+}
