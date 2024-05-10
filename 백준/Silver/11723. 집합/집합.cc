0d8c5313abd48ad7da0932e4c298d4a93bfa39e6
minjiggy <112603809+MaRoili@users.noreply.github.com>
2024-05-10 17:02:24 +0900

    [Silver V] Title: 집합, Time: 708 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -3,16 +3,18 @@
 using namespace std;
 
 int main() {
-	fastio;
-	bitset<21> BIT;
-	int q; cin >> q;
-	while (q--) {
-		string s; int x; cin >> s;
-		if (s == "add") cin >> x, BIT[x] = 1;
-		else if (s == "remove") cin >> x, BIT[x] = 0;
-		else if (s == "check") cin >> x, cout << BIT[x] << '\n';
-		else if (s == "toggle") cin >> x, BIT[x].flip();
-		else if (s == "all") BIT.set();
-		else BIT.reset();
-	}
+    fastio;
+    bitset<21> a;
+    int n;
+    cin>>n;
+    while(n--){
+        string s; int i;
+        cin>>s;
+        if(s== "add") {cin>>i; a[i]=1;}
+        else if (s=="remove") {cin>>i; a[i]=0;}
+        else if (s=="check") {cin>>i; cout<<a[i]<<'\n';}
+        else if (s=="toggle") { cin>>i; a[i].flip();}
+        else if (s=="all") {a.set();}
+        else a.reset();
+    }
 }
