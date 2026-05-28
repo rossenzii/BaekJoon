ce9905e1310ec2a0e14a07e1d13f7dbac5b16b5a
minji jang <jangmj80@naver.com>
2026-05-29 00:23:22 +0900

    [level 2] Title: 순위 검색, Time: 210.49 ms, Memory: 47.5 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
@@ -1,60 +1,54 @@
-#include <bits/stdc++.h>
-#define fastio cin.tie(0)->sync_with_stdio(0)
-#define long long size_t
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
 map<string, vector<int>> scores;
 
-void storeScore(vector<vector<string>> &tmp, int score) {
-    string c1, c2, c3, c4;
-    
-    for (int i=0; i<2; i++) {
-        c1 = tmp[0][i];
-        for (int j=0; j<2; j++) {
-            c2 = tmp[1][j];
-            for (int k=0; k<2; k++) {
-                c3 = tmp[2][k];
-                for (int l=0; l<2; l++) {
-                    c4 = tmp[3][l];
-                    scores[c1 + c2 + c3 + c4].push_back(score);
+void storeScore(vector<vector<string>> &tmp, int score){
+    string c1,c2,c3,c4;
+    for(int i=0; i<2; i++){
+        c1=tmp[0][i];
+        for(int j=0; j<2; j++){
+            c2=tmp[1][j];
+            for(int k=0; k<2; k++){
+                c3=tmp[2][k];
+                for(int l=0; l<2; l++){
+                    c4=tmp[3][l];
+                    scores[c1+c2+c3+c4].push_back(score); // 16 종류 저장
                 }
             }
         }
     }
 }
 
-vector<int> solution(vector<string> info, vector<string> query){
+vector<int> solution(vector<string> info, vector<string> query) {
     vector<int> answer;
-    vector<vector<string>> tmp(4, vector<string>(2,"-"));
-    for(int i=0; i<info.size(); i++){
-        string s;
-        int idx = 0, score = 0;
-        stringstream ss(info[i]);
-        while(ss>>s){
-            if(idx<4) tmp[idx++][0] = s;
-            else score = stoi(s); // 마지막은 숫자니까 정수로 바꿔서 score에 저장
-        }
-        storeScore(tmp, score);
+    for(string s:info){
+        stringstream ss(s);
+        string a,b,c,d; int score;
+        ss >> a >> b >> c >>d >>score;
+        vector<vector<string>> newInfo={
+            {a,"-"},
+            {b,"-"},
+            {c,"-"},
+            {d,"-"}
+        };
+        storeScore(newInfo, score);
     }
-    // 점수 오름차순 정렬
-    for (auto it:scores){
-        sort(scores[it.first].begin(), scores[it.first].end());
+    for(auto &sc:scores){
+        sort(sc.second.begin(), sc.second.end());
     }
-    for(string q: query){
-        string s, key = "";
-        int idx = 0, score = 0;
+    for(string q : query){
         stringstream ss(q);
-        while(ss>>s){
-            if(s=="and") continue;
-            if(idx<4){
-                key+=s;
-                idx++;
-            }else{
-                score = stoi(s);
-            }
-        }
-        auto it = lower_bound(scores[key].begin(), scores[key].end(), score);
-        answer.push_back(scores[key].size() - (it-scores[key].begin()));
+        string a,b,c,d,tmp; int queryScore;
+        ss >> a >> tmp >> b >> tmp >> c >> tmp >> d >> queryScore;
+        string key=a+b+c+d;
+        vector<int> &v=scores[key];
+        int cnt=v.end()-lower_bound(v.begin(), v.end(), queryScore);
+        answer.push_back(cnt);
     }
     return answer;
 }
