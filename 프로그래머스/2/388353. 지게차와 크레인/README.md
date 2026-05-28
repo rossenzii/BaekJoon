06206f22852e7d597661050b0b9c9d6c219eb17f
minji jang <jangmj80@naver.com>
2026-05-28 19:07:37 +0900

    [level 2] Title: 지게차와 크레인, Time: 2.73 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/README.md
+++ 프로그래머스/2/388353. 지게차와 크레인/README.md
@@ -1,10 +1,10 @@
 # [level 2] 지게차와 크레인 - 388353 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388353?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388353) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 3.88 ms
+메모리: 4.07 MB, 시간: 2.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 25일 19:54:17
+2026년 05월 28일 19:07:30
 
 ### 문제 설명
 
--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.cpp
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.cpp
@@ -3,75 +3,132 @@
 #include <queue>
 using namespace std;
 
-bool visited[51][51];
+int dx[4]={-1,1,0,0};
+int dy[4]={0,0,-1,1};
 
-void gCar(char target, vector<string> &storage, int n, int m){
-    int dx[4]={-1,1,0,0};
-    int dy[4]={0,0,1,-1};
-    vector<pair<int,int>> delPoint;
-    //초기화
-    for(int idx=0; idx<n; idx++)
-        fill(visited[idx], visited[idx]+m, false);
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
-            if(i!=0 && i!=n-1 && j!=0 && j!=m-1) continue; // 경계가 아닌 경우는 무시
-            if(storage[i][j]==target) delPoint.push_back(make_pair(i,j)); //삭제 대상에 추가
-            else if (storage[i][j]==' '){ //BFS 시작
-                queue<pair<int,int>> q;
-                q.push(make_pair(i,j));
-                visited[i][j]=true;
-                
-                while(!q.empty()){ //인접한 target 검색
-                    pair<int,int> xy = q.front();
-                    q.pop();
-                    
-                    int x=xy.first; int y=xy.second;
-                    for(int dir=0; dir<4; dir++){
-                        int nx = x+dx[dir], ny=y+dy[dir];
-                        if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
-                        if(storage[nx][ny]==target && !visited[nx][ny]){
-                            delPoint.push_back(make_pair(nx,ny)); // 삭제 대상에 추가
-                            visited[nx][ny]=true;
-                        }
-                        if(storage[nx][ny]==' '&&!visited[nx][ny]){
-                            q.push(make_pair(nx,ny)); //빈곳이면 계속 탐색, 재귀적 연결
-                            visited[nx][ny] = true;
-                        }
-                    }
-                }
-            }
-        }
+bool isOutside(int x, int y,
+               vector<string>& storage,
+               vector<vector<bool>>& outside){
+
+    for(int d=0; d<4; d++){
+        int nx=x+dx[d];
+        int ny=y+dy[d];
+
+        if(outside[nx][ny])
+            return true;
     }
-    for(pair<int,int> point: delPoint){
-        storage[point.first][point.second]=' ';
-    }
+
+    return false;
 }
 
+void makeOutside(vector<string>& board,
+                 vector<vector<bool>>& outside){
 
-void crain(char target, vector<string> &storage, int n, int m){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(storage[i][j]==target) storage[i][j]=' ';
+    int n=board.size();
+    int m=board[0].size();
+
+    queue<pair<int,int>> q;
+
+    q.push({0,0});
+    outside[0][0]=true;
+
+    while(!q.empty()){
+
+        int x=q.front().first;
+        int y=q.front().second;
+        q.pop();
+
+        for(int d=0; d<4; d++){
+
+            int nx=x+dx[d];
+            int ny=y+dy[d];
+
+            if(nx<0 || ny<0 || nx>=n || ny>=m)
+                continue;
+
+            if(outside[nx][ny])
+                continue;
+
+            if(board[nx][ny]!='.')
+                continue;
+
+            outside[nx][ny]=true;
+            q.push({nx,ny});
         }
     }
 }
 
 int solution(vector<string> storage, vector<string> requests) {
-    int answer = 0;
+
     int n=storage.size();
     int m=storage[0].size();
-    for(string cmd : requests)
-    {
-        if(cmd.size()==1)
-            gCar(cmd[0], storage, n, m);
-        else crain(cmd[0], storage, n, m);
-    }
+
+    vector<string> board(n+2, string(m+2,'.'));
+
     for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++)
-            if(storage[i][j] != ' ') answer++;
+        for(int j=0; j<m; j++){
+            board[i+1][j+1]=storage[i][j];
+        }
     }
-    
+
+    for(string req : requests){
+
+        vector<pair<int,int>> removeList;
+
+        vector<vector<bool>> outside(
+            n+2,
+            vector<bool>(m+2,false)
+        );
+
+        makeOutside(board, outside);
+
+        char target=req[0];
+
+        // 지게차
+        if(req.size()==1){
+
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=m; j++){
+
+                    if(board[i][j]!=target)
+                        continue;
+
+                    if(isOutside(i,j,board,outside))
+                        removeList.push_back({i,j});
+                }
+            }
+        }
+
+        // 크레인
+        else{
+
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=m; j++){
+
+                    if(board[i][j]==target)
+                        removeList.push_back({i,j});
+                }
+            }
+        }
+
+        for(auto pos : removeList){
+
+            int x=pos.first;
+            int y=pos.second;
+
+            board[x][y]='.';
+        }
+    }
+
+    int answer=0;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+
+            if(board[i][j]!='.')
+                answer++;
+        }
+    }
+
     return answer;
 }
