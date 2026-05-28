a8e008aa92fd8b4256f22b9b6c2f2479b121b3e8
minji jang <jangmj80@naver.com>
2026-05-28 20:38:37 +0900

    [level 3] Title: 표 편집, Time: 11.68 ms, Memory: 30.4 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.cpp
+++ 프로그래머스/3/81303. 표 편집/표 편집.cpp
@@ -1,52 +1,61 @@
-#include <bits/stdc++.h>
-#define fastio cin.tie(0)->sync_with_stdio(0)
+#include <string>
+#include <stack>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+struct Node{
+    int cur;
+    int prev;
+    int next;
+};
 
-string solution(int n, int k, vector<string> cmd){
+string solution(int n, int k, vector<string> cmd){ // 행개수, 현재행
     vector<int> prev(n), next(n);
-    stack<int> removed;
-    
+    vector<bool> alive(n, true);
     for(int i=0; i<n; i++){
         prev[i]=i-1;
         next[i]=i+1;
     }
-    next[n-1]=-1;
-    int cur=k;
-    
-    for(const string& s: cmd){
-        char command = s[0];
-        int value = 0;
-        if(s.size()>1) value = stoi(s.substr(2));
-        
-        if(command=='U'){
-            while(value--){
-                cur=prev[cur];
+    next[n-1]=-1; // 마지막은 맨 처음으로..
+    stack<Node> removed;
+    for(string let:cmd){
+        if(let[0]=='U'){
+            int num=stoi(let.substr(2));
+            while(num--){
+                k=prev[k];
             }
-        }else if(command=='D'){
-            while(value--){
-                cur=next[cur];
+        }
+        else if(let[0]=='D'){
+            int num=stoi(let.substr(2));
+            while(num--){
+                k=next[k];
             }
-        }else if(command=='C'){
-            removed.push(cur);
-            if(prev[cur]!=-1) next[prev[cur]]=next[cur];
-            if(next[cur]!=-1) prev[next[cur]]=prev[cur];
-            
-            cur = (next[cur] != -1) ? next[cur] : prev[cur];
-        } else if (command == 'Z') {
-            int restore = removed.top();
+        }
+        else if(let[0]=='C'){
+            removed.push({k,prev[k],next[k]});
+            alive[k]=false;
+            if(prev[k]!=-1) next[prev[k]]=next[k]; // 걍 -1은 없다는 거 
+            if(next[k]!=-1) prev[next[k]]=prev[k];
+            if(next[k]!=-1) k=next[k];
+            else k=prev[k];
+        }
+        else if(let[0]=='Z'){
+            Node restore=removed.top();
             removed.pop();
-            if (prev[restore] != -1) next[prev[restore]] = restore;
-            if (next[restore] != -1) prev[next[restore]] = restore;
+            int c=restore.cur;
+            int p=restore.prev;
+            int nx=restore.next;
+            alive[c]=true;
+            if(p!=-1) next[p]=c; // 지금 값이 next로 간다.
+            if(nx!=-1) prev[nx]=c;
+            
         }
     }
-        
-    string answer(n, 'O');
-    while (!removed.empty()) {
-        answer[removed.top()] = 'X';
-        removed.pop();
+    string answer="";
+    for(int i=0; i<n; i++){
+        if(alive[i]) answer+='O';
+        else answer+='X';
     }
-    
     return answer;
-    
 }
