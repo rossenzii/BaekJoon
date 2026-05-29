0d0deaf5d532ef683e81a974374aa409ce869de9
minji jang <jangmj80@naver.com>
2026-05-29 11:36:31 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,12 +1,15 @@
 # 2022년 3월의 오프라인/온라인 상품 판매 데이터의 판매 날짜, 상품ID, 유저ID, 판매량
-select SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
-from ONLINE_SALE 
-where SALES_DATE >='2022-03-01' and SALES_DATE<'2022-04-01'
+
+select sales_date, product_id, user_id, sales_amount
+from online_sale
+where sales_date>='2022-03-01' and sales_date<'2022-04-01'
 
 union all
 
-select SALES_DATE, PRODUCT_ID, null as USER_ID, SALES_AMOUNT
-from OFFLINE_SALE
-where SALES_DATE >='2022-03-01' and SALES_DATE<'2022-04-01'
+select sales_date, product_id, null as user_id, sales_amount
+from offline_sale
+where sales_date>='2022-03-01' and sales_date<'2022-04-01'
 
-order by SALES_DATE asc, PRODUCT_ID asc, USER_ID asc;
+order by sales_date asc, product_id asc, user_id asc;
+
+
