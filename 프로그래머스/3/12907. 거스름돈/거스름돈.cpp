0a040d59baeb2050343da0d900eca5c48e6eb2dd
minji jang <jangmj80@naver.com>
2026-06-14 17:02:33 +0900

    [level 3] Title: 거스름돈, Time: 1.86 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -1,18 +1,16 @@
 #include <string>
 #include <vector>
-
-#define MAX 100010
-#define MODULER 1000000007
+#define MAX 100100
+#define MODULER 10000000007
 using namespace std;
-
 int dp[MAX];
-
-int solution(int n, vector<int> money){
-    dp[0] = 1;
-    for(int i = 0; i < money.size(); i++) {
-        for(int j = money[i]; j <= n; j++) {  
-            dp[j] += dp[j - money[i]];
-            dp[j] %= MODULER;
+int solution(int n, vector<int> money) {
+    dp[0]=1;
+    int answer = 0;
+    for(int i=0; i<money.size(); i++){
+        for(int j=money[i]; j<=n; j++){
+            dp[j]+=dp[j-money[i]];
+            dp[j]%=MODULER;
         }
     }
     return dp[n];
